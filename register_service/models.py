2c14148c5232493503638f8f4f49921cd5c79bc4
Nicola Hauke <jasinai@jasinai.de>
2015-11-11 13:28:48 +0100

    Add identity model
    Adding also a phonenumberfield plugin,
    so we don't have to validate telephonenumbers.

--- register_service/models.py
+++ register_service/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Identity(models.Model):
+    alias = models.CharField(max_length=255, db_index=True, null=False, blank=False)
+    public_key = models.TextField(null=False, blank=False, primary_key=True, db_index=True)
+    drop_url = models.URLField(null=False, blank=False)
+    email = models.EmailField(blank=True, db_index=True)
+    mobile = PhoneNumberField(blank=True, db_index=True)
+    created_at = models.DateTimeField(editable=False)
+    updated_at = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.created_at:
+            self.created_at = timezone.now()
+        self.updated_at = timezone.now()
+        return super(Identity, self).save(*args, **kwargs)
