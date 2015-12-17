b9081c370c6bc4db94061b357d0ffd2d69d40d73
Nicola Hauke <jasinai@jasinai.de>
2015-12-17 11:16:21 +0100

    Change the update API so it works with public_key

--- register_service/test_rest.py
+++ register_service/test_rest.py
@@ -1,5 +1,5 @@
+import base64
 import json
-
 from register_service.models import Identity
 
 
@@ -42,13 +42,17 @@ def test_create_duplicate(api_client, identity):
 
 
 ## UPDATE
-def test_update_identity(api_client, identity):
+def test_update_identity(api_client, identity, public_key):
+    pub_key = str(base64.encodebytes(public_key), encoding='UTF-8')
     response = api_client.post('/api/v0/update/',
                                {'alias': 'foobar',
                                 'drop_url': 'http://127.0.0.1:6000/qabel_user',
                                 'email': 'qabel_user@example.com',
-                                'public_key': 'Qabel'})
+                                'public_key': pub_key})
     assert response.status_code == 200
+    identities = Identity.objects.all()
+    for i in identities:
+        print(repr(i))
     assert Identity.objects.all().count() == 1
     result = loads(response.content)
     identity = Identity.objects.get(alias='foobar')
@@ -57,11 +61,12 @@ def test_update_identity(api_client, identity):
 
 
 def test_invalid_update_identity(api_client, identity):
+    pub_key = str(base64.encodebytes(b'MyPubKey'), encoding='UTF-8')
     response = api_client.post('/api/v0/update/',
                                {'alias': 'foobar',
                                 'drop_url': 'http://127.0.0.1:6000/qabel_user',
                                 'email': 'qabel_user@example.com',
-                                'public_key': 'MyPubKey'})
+                                'public_key': pub_key})
     assert response.status_code == 404
     assert Identity.objects.all().count() == 1
     result = loads(response.content)
@@ -69,3 +74,18 @@ def test_invalid_update_identity(api_client, identity):
     old_identity = Identity.objects.get(alias='qabel_user')
     assert "qabel_user" == old_identity.alias
     assert "qabel_user@example.com" != old_identity.email
+
+def test_update_without_public_key(api_client, identity):
+    response = api_client.post('/api/v0/update/',
+                               {'alias': 'foobar',
+                                'drop_url': 'http://127.0.0.1:6000/qabel_user',
+                                'email': 'qabel_user@example.com'})
+    assert response.status_code == 404
+    assert Identity.objects.all().count() == 1
+    result = loads(response.content)
+    assert result == "The public key is required."
+    old_identity = Identity.objects.get(alias='qabel_user')
+    assert "qabel_user" == old_identity.alias
+    assert "qabel_user@example.com" != old_identity.email
+
+
