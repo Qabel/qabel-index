689b9b22f3efd2b7be438f4595825e747a616cd2
Marian Beermann <beermann@qabel.de>
2016-09-26 09:23:54 +0200

    fix requirements

--- qabel_index/settings.py
+++ qabel_index/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_prometheus',
-    'mail_templated',
     'rest_framework',
     'sendsms',
     'bootstrapform',
